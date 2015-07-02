33c618850dc0d13eb7ceaa775094a6fa3985e744
rblatss@gmail.com <rblatss@gmail.com>
2015-07-02 13:09:27 -0700

    Script reads queries from txt file and write output to text file for each query.

--- jobsearch.py
+++ jobsearch.py
@@ -1,113 +1,90 @@
 ï»¿''' ****************************************************************
 
-							Get Job Titles 
-  ----------------------------------------------------------------
-  I should have written this at the start of my job search. Pretty
-  simple idea, but useful. This script automates a search for job openings 
-  on indeed.com.
-    
-  At this point, the script only verifies that the title has a combination
-  of software/electrical and engineer/developer.
-  
-  Later, i'll parse the job descriptions for specific requirements
-  (c++, python, linux, unix, related degree, electrical engineering
-   etc.)
-  
-  Procedure (4 steps):
-    1) search "company name" on indeed.com
-	
-		btw: url dissected
-		http://www.youtube.com/watch?v=DBJ7mBxi8LM&list=FLjtfXY-PoLnBtJTrAVIHR3A&index=5
-
-			Protocol – http://
-			
-			Subdomain – www.
-			
-			Domain – youtube.com/
-			
-			Path to file – watch
-			
-			Query string – ?v=DBJ7mBxi8LM&list=FLjtfXY-PoLnBtJTrAVIHR3A&index=5
-			(name/value pairs!!)
-	
-		
-		From:
-			http://maps.google.com/maps?q=62+4th+St+NW,+Winter+Haven,+FL&hl=en&sll=27.698638,-83.804601&sspn=22.576023,34.453125&oq=62+4th+st+nw,+&hnear=62+4th+St+NW,+Winter+Haven,+Florida+33881&t=m&z=17
-		To:
-			http://maps.google.com/maps?q=62+4th+St+NW,+Winter+Haven,+FL
-		
-		* These are the same
-	
-	2) skim thru position titles and print matches
+						Find a Job via Indeed
+		-----------------------------------------------------
+  Procedure:
+    0) read query name and search it via indeed.com	
+    1) create output txt file for query
+	2) skim thru position titles and write to text
 	3) go to next page
 	4) repeat prev 2 until no more next
-  
-  to do:
-
-	2) validate that company of job posting is company searched
-		maybe try regex to look for title w/o certain postfix/prefix
-		instead of searching for affirmative
-	3) parse job descriptions and match based on job requirements
-	  
-  This script was written using Notepad++ (text, block manipulation,
-  autoindent etc.), command prompt (pass args to script) and IDLE
-  (test individual functions).
-
+    5) repeat 0 - 4 for all queries
+	
 **************************************************************** '''
 
 from bs4 import BeautifulSoup
-import re, requests, sys
-
-# Get args
-if(len(sys.argv) < 1):
-	print "Format: python jobsearch.py [company name]"
-	sys.exit(0)
-
-# Request Indeed.com
-keys = ['q', 'l']
-values = [ ('+'.join(sys.argv[1:]) ), '98136']
-query = dict(zip(keys, values))
-
-search_url = "https://www.indeed.com"
-
-res = requests.get(search_url + "/jobs", query)
-
-# Criteria for a match (job title, correct company name)
-main = r"[A-z0-9\s]*(([Ss]oftware)|([Ee]lectrical))+\s?(Development)?\s?([Ee]ngineer|[Dd]eveloper)+[A-z0-9\s]*"
-jobtitle = re.compile(main)
+import os, re, requests, sys
 
 # Function used to replace %2B w/ + in URL
 def CorrectURL(url):
 	return "+".join(url.split("%2B"))
 
-# Iterate thru all pages of search results and print job titles
-i = 1
-next_page = True
-while(next_page):
-	
+def main():
 	try:
-		res.raise_for_status() # halt if content is bad (raises exception)
-	except Exception as exc:
-		print('There was a problem: %s' % (exc))
-		sys.exit(0)
+		os.mkdir('jobs')
+	except OSError:
+		pass
 	
-	soup = BeautifulSoup(res.text)
+	# Read query names
+	file_handle = open("queries.txt", 'r')
+	queries_list = file_handle.readlines()
+	file_handle.close()
 	
-	# Find matches in job post titles
-	print "Page " + str(i) + " results:"
-	print "-----------------------------"
-	for tag in soup.find_all('h2', attrs = {'class' : 'jobtitle'}):
-		a = tag.findChild('a')
-		if a['itemprop'] != [] and a['itemprop'] == 'title':
-			if re.match(jobtitle, a.text) != None:
-				print a.text.encode("utf8") + " - " + tag.next_sibling.next_sibling.text
+	for query in queries_list:
+		query = query[:-1] # throw out '\n'
+		
+		# Request Indeed.com
+		
+		''' keys = ['q', 'l']
+		values = [ '+'.join(query.split(' ')), '98136']
+		complete_query = dict(zip(keys, values)) '''
+		search_url = "https://www.indeed.com"
+		
+		# function did bad stuff when passed query as arg 2
+		res = requests.get(search_url + "/jobs?q=" + '+'.join(query.split(' ')) + '&l=98136')
+		
+		# Criteria for a match
+		jobtitle = re.compile(r"(.*)(([Ss]oftware)|([Ee]lectrical)|([Ff]irmware))+\s?(Development)?\s?([Ee]ngineer|[Dd]eveloper)+(.*)")
+		
+		# Iterate thru all pages of search results and write job titles to .txt file
+		i = 1
+		next_page = True
 	
-	# Go to next page
-	if soup.find_all('span', text=re.compile(r'^Next')) != []: # verify there's a next page
-		next_url = CorrectURL(search_url + soup.find_all('span', text=re.compile(r'^Next'))[0].parent['href'])
-		res = requests.get(next_url)
-	else:
-		next_page = False
-	i += 1
-
-print "Search ended."
+		output = open(os.path.join('jobs', query + ".txt"), 'w')
+		while(next_page):
+			try:
+				res.raise_for_status() # halt if content is bad
+			except Exception as exc:
+				print('There was a problem: %s' % (exc))
+				sys.exit(0)
+			
+			soup = BeautifulSoup(res.text)
+			
+			# Find matches in job post titles and write to output file
+			output.write("Page " + str(i) + " results:\n")
+			output.write("-----------------------------\n")
+			for tag in soup.find_all('h2', attrs = {'class' : 'jobtitle'}):
+				a = tag.findChild('a')
+				if a['itemprop'] != [] and a['itemprop'] == 'title':
+					if re.match(jobtitle, a.text) != None:
+						output.write(a.text.encode('utf8','ignore') + " - " + tag.next_sibling.next_sibling.text.encode('utf8', 'ignore') + "\n")
+			
+			# Go to next page
+			if soup.find_all('span', text=re.compile(r'^Next')) != []: # verify there's a next page
+				try:
+					next_url = CorrectURL(search_url + soup.find_all('span', text=re.compile(r'^Next'))[0].parent['href'])
+					res = requests.get(next_url)
+				except KeyError:
+					print "Incomplete search for " + query + " at page " + str(i)
+					next_page = False
+					continue
+			else:
+				next_page = False
+			i += 1
+		
+		output.close()
+	
+	print "Search ended."
+	
+if __name__ == "__main__":
+	main()
