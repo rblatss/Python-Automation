b0de696d0bb15b3bf2f9748f7a0e61447ab1ab51
rblatss@gmail.com <rblatss@gmail.com>
2015-07-20 12:29:52 -0700

    Added Rename.py

---
+++ rename.py
@@ -0,0 +1,10 @@
+﻿import os, re
+
+dir = os.getcwd()
+pattern = re.compile(r'^cci')
+filenames = os.listdir(dir)
+for n in filenames:
+  if re.match(pattern, n):
+    os.rename(n, "ctci" + n[3:])
+ 
+
